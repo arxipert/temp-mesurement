13ae69e5b2bffa2bd7bbdb5fa8fa6cef0bd3274e
HailToSG <needyak@yandex.ru>
2020-04-03 23:00:11 +0300

    Initial commit

---
+++ src/main/java/com/hailtosg/tempmesurement/TempMesurementApplication.java
@@ -0,0 +1,13 @@
+package com.hailtosg.tempmesurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempMesurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempMesurementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hailtosg/tempmesurement/TempMesurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hailtosg.tempmesurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempMesurementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
